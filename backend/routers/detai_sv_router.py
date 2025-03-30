20a7a273eaf01e9cbe9f942cfefa92d64db9838d
Lamb <100024864+Lambbro@users.noreply.github.com>
2025-03-30 15:28:19 +0700

    Add files via upload

--- backend/routers/detai_sv_router.py
+++ backend/routers/detai_sv_router.py
@@ -7,25 +7,25 @@ from auths.auth import get_current_user
 
 from services.detai_sv_service import DangKyNCKHService
 
-router = APIRouter(
+dtsv_router = APIRouter(
     prefix="/nckh",
     tags=["Nghiên cứu khoa học"]
 )
-@router.get("/", response_model=List[detai_sv_schemas.DangKyNCKHResponse])
+@dtsv_router.get("/", response_model=List[detai_sv_schemas.DangKyNCKHResponse])
 async def get_all(skip: int = 0, limit: int = 100, 
                   db: AsyncSession = Depends(get_db), 
                   current_user: dict = Depends(get_current_user)):
     service = DangKyNCKHService(db, current_user)
     return await service.get_all(skip, limit)
 
-@router.get("/{ma_dk}", response_model=detai_sv_schemas.DangKyNCKHResponse)
+@dtsv_router.get("/{ma_dk}", response_model=detai_sv_schemas.DangKyNCKHResponse)
 async def get_dk(ma_dk: int, 
                  db: AsyncSession = Depends(get_db), 
                  current_user: dict = Depends(get_current_user)):
     service = DangKyNCKHService(db, current_user)
     return await service.get(ma_dk)
 
-@router.post("/", response_model=detai_sv_schemas.DangKyNCKHResponse)
+@dtsv_router.post("/", response_model=detai_sv_schemas.DangKyNCKHResponse)
 async def create_dk(dang_ky: detai_sv_schemas.DangKyNCKHCreate, 
                     list_nguyen_vong: List[detai_sv_schemas.NguyenVongDKCreate], 
                     db: AsyncSession = Depends(get_db), 
@@ -33,7 +33,7 @@ async def create_dk(dang_ky: detai_sv_schemas.DangKyNCKHCreate,
     service = DangKyNCKHService(db, current_user)
     return await service.create(dang_ky, list_nguyen_vong)
 
-@router.put("/{ma_dk}", response_model=List[detai_sv_schemas.NguyenVongDKResponse])
+@dtsv_router.put("/{ma_dk}", response_model=List[detai_sv_schemas.NguyenVongDKResponse])
 async def update_dk(ma_dk: int, 
                     list_nv: List[detai_sv_schemas.NguyenVongDKUpdate], 
                     db: AsyncSession = Depends(get_db), 
@@ -41,9 +41,9 @@ async def update_dk(ma_dk: int,
     service = DangKyNCKHService(db, current_user)
     return await service.update(ma_dk, list_nv)
 
-@router.delete("/{ma_dang_ky}")
-async def delete_dk(ma_dang_ky: int, 
+@dtsv_router.delete("/{ma_dang_ky}")
+async def delete_dk(ma_dk: int, 
                     db: AsyncSession = Depends(get_db), 
                     current_user: dict = Depends(get_current_user)):
     service = DangKyNCKHService(db, current_user)
-    return await service.delete(ma_dang_ky)
+    return await service.delete(ma_dk)
--- backend/routers/quan_ly_router.py
+++ backend/routers/quan_ly_router.py
@@ -5,11 +5,11 @@ from schemas import base_schemas as schemas
 from typing import List
 from auths.auth import get_current_user
 from services import (
-    ql_khoa_service, ql_khenthuong_service, ql_hnc_service,
+    ql_khoa_service, ql_hnc_service,
     ql_sv_service, ql_gv_service
 )
 
-router = APIRouter(
+ql_router = APIRouter(
     prefix="/quan-ly",
     tags=["Quản Lý"]
 )
@@ -19,7 +19,7 @@ router = APIRouter(
 # ==========================
 
 #Khoa
-@router.get("/khoa", response_model=list[schemas.KhoaResponse])
+@ql_router.get("/khoa", response_model=list[schemas.KhoaResponse])
 async def xem_danh_sach_khoa(
     current_user: dict = Depends(get_current_user),
     db: AsyncSession = Depends(get_db)
@@ -28,7 +28,7 @@ async def xem_danh_sach_khoa(
     return await service.get_all_khoa()
 
 # Thêm khoa mới
-@router.post("/khoa/them", response_model=schemas.KhoaResponse)
+@ql_router.post("/khoa/them", response_model=schemas.KhoaResponse)
 async def them_khoa(
     khoa: schemas.KhoaCreate, 
     current_user: dict = Depends(get_current_user),
@@ -38,7 +38,7 @@ async def them_khoa(
     return await service.create_khoa(khoa)
 
 # Cập nhật khoa
-@router.put("/khoa/cap-nhat/{ma_khoa}", response_model=schemas.KhoaResponse)
+@ql_router.put("/khoa/cap-nhat/{ma_khoa}", response_model=schemas.KhoaResponse)
 async def cap_nhat_khoa(
     ma_khoa: str,
     khoa: schemas.KhoaUpdate,
@@ -49,7 +49,7 @@ async def cap_nhat_khoa(
     return await service.update_khoa(ma_khoa, khoa)
 
 # Xóa khoa
-@router.delete("/khoa/xoa/{ma_khoa}", response_model=dict)
+@ql_router.delete("/khoa/xoa/{ma_khoa}", response_model=dict)
 async def xoa_khoa(
     ma_khoa: str,
     current_user: dict = Depends(get_current_user),
@@ -62,7 +62,7 @@ async def xoa_khoa(
 # 📌 QUẢN LÝ KHOA
 # ==========================
 #Hướng nghiên cứu
-@router.get("/hnc", response_model=list[schemas.HuongNghienCuuResponse])
+@ql_router.get("/hnc", response_model=list[schemas.HuongNghienCuuResponse])
 async def get_all_huong_nghien_cuu(
     db: AsyncSession = Depends(get_db),
     current_user: dict = Depends(get_current_user)
@@ -70,7 +70,7 @@ async def get_all_huong_nghien_cuu(
     service = ql_hnc_service.QLHuongNghienCuuService(db, current_user)
     return await service.get_all_huong_nghien_cuu()
 
-@router.get("/hnc/{ma_hnc}", response_model=schemas.HuongNghienCuuResponse)
+@ql_router.get("/hnc/{ma_hnc}", response_model=schemas.HuongNghienCuuResponse)
 async def get_huong_nghien_cuu(
     ma_hnc: int, 
     db: AsyncSession = Depends(get_db),
@@ -79,7 +79,7 @@ async def get_huong_nghien_cuu(
     service = ql_hnc_service.QLHuongNghienCuuService(db, current_user)
     return await service.get_huong_nghien_cuu(ma_hnc)
 
-@router.post("/hnc/create", response_model=schemas.HuongNghienCuuResponse, status_code=201)
+@ql_router.post("/hnc/create", response_model=schemas.HuongNghienCuuResponse, status_code=201)
 async def create_huong_nghien_cuu(
     hnc_data: schemas.HuongNghienCuuCreate, 
     db: AsyncSession = Depends(get_db),
@@ -88,7 +88,7 @@ async def create_huong_nghien_cuu(
     service = ql_hnc_service.QLHuongNghienCuuService(db, current_user)
     return await service.create_huong_nghien_cuu(hnc_data)
 
-@router.put("/hnc/update/{ma_hnc}", response_model=schemas.HuongNghienCuuResponse)
+@ql_router.put("/hnc/update/{ma_hnc}", response_model=schemas.HuongNghienCuuResponse)
 async def update_huong_nghien_cuu(
     ma_hnc: int, 
     hnc_data: schemas.HuongNghienCuuUpdate, 
@@ -98,7 +98,7 @@ async def update_huong_nghien_cuu(
     service = ql_hnc_service.QLHuongNghienCuuService(db, current_user)
     return await service.update_huong_nghien_cuu(ma_hnc, hnc_data)
 
-@router.delete("/hnc/delete/{ma_hnc}", response_model=dict)
+@ql_router.delete("/hnc/delete/{ma_hnc}", response_model=dict)
 async def delete_huong_nghien_cuu(
     ma_hnc: int, 
     db: AsyncSession = Depends(get_db),
@@ -111,7 +111,7 @@ async def delete_huong_nghien_cuu(
 # 📌 QUẢN LÝ SINH VIÊN
 # ==========================
 
-@router.get("/sinh-vien/{ma_sv}", response_model=schemas.SinhVienResponse)
+@ql_router.get("/sinh-vien/{ma_sv}", response_model=schemas.SinhVienResponse)
 async def xem_sinh_vien(
     ma_sv: str,
     current_user: dict = Depends(get_current_user),
@@ -120,7 +120,7 @@ async def xem_sinh_vien(
     service = ql_sv_service.QLSinhVienService(db, current_user)
     return await service.xem_sv(ma_sv)
 
-@router.get("/sinh-vien", response_model=List[schemas.SinhVienResponse])
+@ql_router.get("/sinh-vien", response_model=List[schemas.SinhVienResponse])
 async def xem_danh_sach_sv(
     skip: int = 0,       # Mặc định bỏ qua 0 bản ghi
     limit: int = 100,    # Mặc định lấy 100 bản ghi mỗi lần
@@ -131,7 +131,7 @@ async def xem_danh_sach_sv(
     return await service.xem_danh_sach_sv(skip, limit)
 
 # Đăng ký một sinh viên
-@router.post("/sinh-vien/them", response_model=schemas.SinhVienResponse)
+@ql_router.post("/sinh-vien/them", response_model=schemas.SinhVienResponse)
 async def tao_sinh_vien(
     sinh_vien: schemas.SinhVienCreate, 
     current_user: dict = Depends(get_current_user),
@@ -141,7 +141,7 @@ async def tao_sinh_vien(
     return await service.tao_sinh_vien(sinh_vien)
 
 # Xóa sinh viên theo mã
-@router.delete("/sinh-vien/xoa/{ma_sv}", response_model=dict)
+@ql_router.delete("/sinh-vien/xoa/{ma_sv}", response_model=dict)
 async def xoa_sinh_vien(
     ma_sv: str,
     current_user: dict = Depends(get_current_user),
@@ -151,7 +151,7 @@ async def xoa_sinh_vien(
     return await service.xoa_sinh_vien(ma_sv)
 
 # Đăng ký danh sách sinh viên
-@router.post("/sinh-vien/dang-ky", response_model=dict)
+@ql_router.post("/sinh-vien/dang-ky", response_model=dict)
 async def tao_danh_sach_sinh_vien(
     danh_sach_sinh_vien: list[schemas.SinhVienCreate], 
     current_user: dict = Depends(get_current_user),
@@ -161,7 +161,7 @@ async def tao_danh_sach_sinh_vien(
     return await service.tao_danh_sach_sinh_vien(danh_sach_sinh_vien)
 
 # Cập nhật thông tin sinh viên
-@router.put("/sinh-vien/cap-nhat/{ma_sv}", response_model=schemas.SinhVienResponse)
+@ql_router.put("/sinh-vien/cap-nhat/{ma_sv}", response_model=schemas.SinhVienResponse)
 async def cap_nhat_sinh_vien(
     ma_sv: str,
     sinh_vien_update: schemas.SinhVienUpdate,
@@ -176,7 +176,7 @@ async def cap_nhat_sinh_vien(
 # ==========================
 
 # Thêm giảng viên
-@router.post("/giang-vien/them", response_model=schemas.GiangVienResponse)
+@ql_router.post("/giang-vien/them", response_model=schemas.GiangVienResponse)
 async def tao_giang_vien(
     giang_vien: schemas.GiangVienCreate, 
     current_user: dict = Depends(get_current_user),
@@ -186,7 +186,7 @@ async def tao_giang_vien(
     return await service.tao_giang_vien(giang_vien)
 
 # Xóa giảng viên
-@router.delete("/giang-vien/xoa/{ma_gv}", response_model=dict)
+@ql_router.delete("/giang-vien/xoa/{ma_gv}", response_model=dict)
 async def xoa_giang_vien(
     ma_gv: str,
     current_user: dict = Depends(get_current_user),
@@ -196,7 +196,7 @@ async def xoa_giang_vien(
     return await service.xoa_giang_vien(ma_gv)
 
 # Cập nhật giảng viên
-@router.put("/giang-vien/cap-nhat/{ma_gv}", response_model=schemas.GiangVienResponse)
+@ql_router.put("/giang-vien/cap-nhat/{ma_gv}", response_model=schemas.GiangVienResponse)
 async def cap_nhat_giang_vien(
     ma_gv: str,
     giang_vien_update: schemas.GiangVienUpdate,
@@ -206,7 +206,7 @@ async def cap_nhat_giang_vien(
     service = ql_gv_service.QLGiangVienService(db, current_user)
     return await service.cap_nhat_giang_vien(ma_gv, giang_vien_update)
 
-@router.get("/giang-vien/{ma_gv}", response_model=schemas.GiangVienResponse)
+@ql_router.get("/giang-vien/{ma_gv}", response_model=schemas.GiangVienResponse)
 async def xem_giang_vien(
     ma_gv: str,
     current_user: dict = Depends(get_current_user),
@@ -215,7 +215,7 @@ async def xem_giang_vien(
     service = ql_gv_service.QLGiangVienService(db, current_user)
     return await service.xem_giang_vien(ma_gv)
 
-@router.get("/giang-vien", response_model=list[schemas.GiangVienResponse])
+@ql_router.get("/giang-vien", response_model=list[schemas.GiangVienResponse])
 async def lay_danh_sach_giang_vien(
     current_user: dict = Depends(get_current_user),
     db: AsyncSession = Depends(get_db)
